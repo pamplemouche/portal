31d5fd511d0c88c70121d83dea10583af5e5606a
pamplemouche <pamplemouchetech@gmail.com>
2025-02-23 03:09:34 +0100

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+  <head>
+    <title>Créer un compte</title>
+  </head>
+  <body>
+    <form>
+      <label>Nom</label>
+      <input type="text" placeholder="Votre Nom">
+    </form>
+  </body>
+</html>
